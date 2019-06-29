90d3b44d2b7dbb5c35d76f97680f9b01b1934796
Trombiko <michal.trombik@gmail.com>
2019-06-29 15:35:37 +0200

    String Calculator Step 1: Added Test Class and comments in codes

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,18 +1,24 @@
 public class StringCalculator
 {
 
-    public static int Add(String numbers) {
+    public static int Add(String numbers)
+    {
         String[] charstab = numbers.split(",");
 
-        if (numbers.isEmpty()) {
+        if (numbers.isEmpty())  // Checking if we have empty string that we return 0
+        {
             return 0;
         }
-        else if(numbers.length() == 1)
+        else if(numbers.length() == 1) // If we have 1 value we have to return the same values we get
         {
             return Integer.parseInt(numbers);
-        }else {
-            return Integer.parseInt(charstab[0]) + Integer.parseInt(charstab[1]);
+        }else // If we have 2 values we have to return sum
+        {
+            return Integer.parseInt(charstab[0]) + Integer.parseInt(charstab[1]); // adding 2 input values
         }
+
+
+
 
     }
 
---
+++ src/StringCalculatorTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @Before // method which must be called BEFORE tests in class (things important for tests)
+    public void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void nothingInStringReturnZero() // Check that if we pass empty string that we will get 0
+    {
+        assertEquals("String is empty", 0, stringCalculator.Add(""));
+    }
+
+    @Test
+    public void singleValueInputTheSameValueOutput() // For example: Input: "1" so Output must equals: 1
+    {
+        assertEquals(stringCalculator.Add("1") , 1); // Values comes from task 1
+    }
+
+    @Test
+    public void twoValuesInputAndSingleSumOutput() // when we get 2 inputs numbers and we want sum in output
+    {
+        assertEquals(stringCalculator.Add("1,2"), 3); // Values comes from task 1
+    }
+
+
+
+
+    
+}

fee30198612f718aaf3ea8266405822aeb511b88
Trombiko <michal.trombik@gmail.com>
2019-06-30 13:59:26 +0200

    String Calculator Step 7: Delimiters can be of any length with the following format: “//[delimiter]\n” for example: “//[***]\n1***2***3” should return 6

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,43 +1,50 @@
 public class StringCalculator {
 
+
+
     public static int Add(String numbers) throws Exception {
-        String[] charstab = numbers.split(";|,|\n"); // numbers spliting by ',' or '\n' or ';'
+        String delimiter = ";|,|\n"; // numbers will be split by ',' or '\n' or ';' using DELIMETER
+        String[] charstab = numbers.split(delimiter);
 
         String negatives = ""; // make a new empty string
-        for (int i = 0; i<charstab.length; i++)
-        {
+        for (int i = 0; i < charstab.length; i++) {
             if (charstab[i].contains("-")) // when we get negative number we add it to new string
                 negatives += "," + charstab[i]; // add to string
         }
-        if (!negatives.isEmpty()) throw new IllegalArgumentException("Negatives not allowed: " + negatives.substring(1)); // show in Expection negative numbers
+        if (!negatives.isEmpty()) // if negative inputs are in String
+            throw new IllegalArgumentException("Negatives not allowed: " + negatives.substring(1)); // show in Expection negative numbers
 
 
-
-
-        if(numbers.startsWith("//")||numbers.startsWith("\n")) // if it starts with '//' or '\n' do that
+        if (numbers.startsWith("//") || numbers.startsWith("\n")) // if it starts with '//' or '\n' do that
         {
+            int delimiterNumber;
+            if(numbers.startsWith("//[")) // if it starts with '//[' we look for delimiter inside [DELIMITER]
+            {
+                delimiterNumber = numbers.indexOf("//[") + 3; // place from start where we find DELIMITER
+                delimiter = numbers.substring(delimiterNumber,numbers.indexOf("]"));// when DELIMITER should be finished
+            }
             String[] parts = numbers.split("\n"); // split it when its new line
             String part2 = parts[1]; // we need second[1] part, because there are numbers
-            String[] charstab2 = part2.split(";|,|\n");
+            String[] charstab2 = part2.split(delimiter); // find numbers spliting by DELIMITERS
 
             int suma = 0;
-            for (int j = 0; j < charstab2.length; j++) {
+            for (int j = 0; j < charstab2.length; j++)
+            {
                 suma += Integer.parseInt(charstab2[j]);  // adding the following numbers we get on input
             }
-            return suma;
-        }else {
+            return suma; // return sum of input numbers
+        } else {
             if (numbers.isEmpty())  // Checking if we have empty string that we return 0
             {
                 return 0;
-            } else if (numbers.length() == 1  && Integer.parseInt(numbers) >= 0) // If we have 1 value we have to return the same values we get
+            } else if (numbers.length() == 1 && Integer.parseInt(numbers) >= 0) // If we have 1 value we have to return the same values we get
             {
                 return Integer.parseInt(numbers); // return the same value what we get on input
             } else // If we have more then 1 value we have to return sum of them
             {
                 int sum = 0;
-                for (int i = 0; i < charstab.length; i++)
-                {
-                    if(Integer.parseInt(charstab[i]) < 1001) // we ignore values > 1000
+                for (int i = 0; i < charstab.length; i++) {
+                    if (Integer.parseInt(charstab[i]) < 1001) // we ignore values > 1000
                         sum += Integer.parseInt(charstab[i]);  // adding the following numbers we get on input only if number is lower than 1001
                 }
                 return sum; // return sum of input numbers
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -34,24 +34,31 @@ public class StringCalculatorTest {
     @Test
     public void unknownAmountOfInputNumbers() throws Exception // Checking Add method, if it handle an unknown amount of numbers and using NEW LINES and others delimiters
     {
-        assertEquals(stringCalculator.Add("1;2,3") , 6); // test for 3 input values when we use NEW LINE
-        assertEquals(stringCalculator.Add("//\n4,5,6,7") , 22); // test for 4 input values starting with //
-        assertEquals(stringCalculator.Add("\n4,5,6") , 15); // test for 3 input values starting with \n
+        assertEquals(stringCalculator.Add("1,2,3") , 6); // test for 3 input values
+        assertEquals(stringCalculator.Add("//\n5,6,7") , 18); // test for 4 input values starting with // and NEW LINE
+        assertEquals(stringCalculator.Add("\n4;5,6") , 15); // test for 3 input values starting with \n
     }
 
-
+/*
     @Test
     public void checkNegativesNumbers() throws Exception // Checking Add method with negatives numbers
     {
         assertEquals(stringCalculator.Add("-1,2,3,4"), 8); // test for 4 input values include -1 on start
         assertEquals(stringCalculator.Add("4,-5,6") , 5); // test for 3 input values include negative number in the middle
     }
-
+*/
 
     @Test
     public void weCanUseOnlyLessThanThousandValues() throws Exception // Check that we can use only values < 1001
     {
         assertEquals(stringCalculator.Add("1001,2"), 2); // 1001 should be ignored
+    }
+
+    @Test
+    public void testDifferentLengthOfDelimiter() throws Exception//Step 7 - testing different length of delimiter
+    {
+        assertEquals(stringCalculator.Add("//[!!!]\n2!!!8!!!10"),20); // Check our DELIMITER(3 chars) in [] is correct
+        assertEquals(stringCalculator.Add("//[%%%%]\n20%%%%10%%%%25"),55); // Check our DELIMITER(4 chars) in [] is correct
     }
 
 }
